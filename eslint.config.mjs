0e3677a95afda94d8c190b1af7da693145351547
alexanderfcd <frontcodejunior@gmail.com>
2025-06-19 22:01:05 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
 	// matches all files ending with .js
 	{
-		files: ["**/*.js"],
+		"files": ["**/*.js"],
 		rules: {
 			semi: "error",
 		},
