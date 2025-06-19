afd8408989be7dd85f180fda889fc13b7c392523
alexanderfcd <frontcodejunior@gmail.com>
2025-06-19 23:48:22 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 // eslint.config.js
-import { defineConfig } from "eslint/config";
+ 
 
 
-export default defineConfig([
+ [
 	{
 		files: ["**/*.js"],
 		rules: {
 			semi: "error"
 		}
 	}
-]);
+] 
